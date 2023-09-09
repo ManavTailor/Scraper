1bc33a121df731b4f3272f0720ca62287a0398d0
Manav Tailor <tailormanav68@gmail.com>
2023-09-09 13:46:24 +0530

    csv

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const { writeFile } = require('fs/promises');
 const cheerio = require('cheerio');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -10,13 +11,13 @@ const cheerio = require('cheerio');
   const $ = cheerio.load(pageContent);
 
   const productsData = [];
-  
+
   $('div.product-grid__items .product-card').each((index, element) => {
     const productName = $(element).find('.product-card__title').text().trim();
     const productPrice = $(element).find('.product-card__animation_wrapper').text().trim();
     const productDescription = $(element).find('.product-card__subtitle').text().trim();
     const productImage = $(element).find('.product-card__hero-image').attr('src');
-    
+
     productsData.push({
       name: productName,
       description: productDescription,
@@ -25,8 +26,18 @@ const cheerio = require('cheerio');
     });
   });
 
-  await writeFile('product.json', JSON.stringify(productsData, null, 4)); 
+  await writeFile('products.json', JSON.stringify(productsData, null, 4));
+  const csvWriter = createCsvWriter({
+    path: 'products.csv',
+    header: [
+      { id: 'name', title: 'Name' },
+      { id: 'description', title: 'Description' },
+      { id: 'price', title: 'Price' },
+      { id: 'image', title: 'Image' },
+    ],
+  });
+
+  await csvWriter.writeRecords(productsData);
 
   await browser.close();
-
 })();
